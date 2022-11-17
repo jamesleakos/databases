17b096cc538ac0b5d664657c1a16c3dc559c2cad
James Leakos <jamesleakos@gmail.com>
2022-11-17 13:18:56 -0700

    added friends

---
+++ client/friends.js
@@ -0,0 +1,30 @@
+// This object houses all the friend _data_ for the app.
+// Treat it like a data structure - add methods to interact
+// with and manipulate the data.
+
+var Friends = {
+  // TODO: Define how you want to store your list of friends.
+
+  _data: {},
+  loadUsers: function(messages) {
+    for (let message of messages) {
+      if (!Friends._data[message.username]) {
+        Friends._data[message.username] = false;
+      }
+    }
+  },
+  toggleStatus: function(friend) {
+    Friends._data[friend] = !Friends._data[friend];
+    console.log(Friends._data);
+  },
+  isFriend: function (friend) {
+    return Friends._data[friend];
+  }
+
+
+
+
+  // TODO: Define methods which allow you to add, toggle,
+  // and check the friendship status of other users.
+
+};
